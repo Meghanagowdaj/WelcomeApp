d666829a89b32edffd19247f6a2e23a18b986e8d
Megha P J <meghajayram30@gmail.com>
2024-02-18 22:25:36 +0530

    final commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using WelcomeAppNew;
+
+Console.WriteLine("wlcome to authenticate app");
+SignIn obj = new SignIn();
+Console.WriteLine(obj.Authenticate("admin", "admin@1256"));
---
+++ SignIn.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeAppNew
+{
+    public class SignIn
+    {
+        public string Authenticate(string userName, string password)
+        {
+#pragma warning disable CS0219 // Variable is assigned but its value is never used
+            string msg;
+#pragma warning restore CS0219 // Variable is assigned but its value is never used
+            if ((string.IsNullOrEmpty(userName)) || string.IsNullOrEmpty(password))
+            {
+                msg = "User name and password required";
+            }
+            else
+            {
+                if ((userName == "admin") && (password == "admin@1256"))
+                {
+                    msg = "Authenticate pass";
+                }
+                else
+                {
+                    msg = "Authenticate fail";
+                }
+
+            }
+            return "Provide user details";
+        }
+    }
+
+}
+
+
